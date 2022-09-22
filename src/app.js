109b374350c55042cfd7689b88d0ce05377a3058
Rafiat Salewa <idrisrafiat91@gmail.com>
2022-09-22 16:36:01 +0100

    showing the current temperature and time

---
+++ src/app.js
@@ -0,0 +1,50 @@
+let currentTime = new Date();
+let time = document.querySelector("#time");
+let Days = [
+  "Sunday",
+  "Monday",
+  "Tueday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = Days[currentTime.getDay()];
+let hours = currentTime.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = currentTime.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+time.innerHTML = `${day} ${hours}:${minutes}`;
+
+function showTemp(response) {
+  console.log(response.data);
+  let currentCity = document.querySelector("#city");
+  let tempElement = document.querySelector("#temperature");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let description = document.querySelector("#description");
+  currentCity.innerHTML = response.data.name;
+  tempElement.innerHTML = Math.round(response.data.main.temp);
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = response.data.wind.speed;
+  description.innerHTML = response.data.weather[0].description;
+}
+function search(city) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemp);
+}
+function searchCity(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  search(city);
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", searchCity);
+
+search("Abuja");
