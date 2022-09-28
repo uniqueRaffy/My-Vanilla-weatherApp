64b9ea65bf7e8ae48c47dfc8150646334be41777
Rafiat Salewa <idrisrafiat91@gmail.com>
2022-09-28 10:11:42 +0100

    updated the JS file

--- src/app.js
+++ src/app.js
@@ -22,7 +22,8 @@ function formatDate(timestamp) {
   time.innerHTML = `${day} ${hours}:${minutes}`;
 }
 
-function showForecast() {
+function showForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
   let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
@@ -49,8 +50,14 @@ function showForecast() {
   forecastElement.innerHTML = forecastHTML;
 }
 
+function displayForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "5863935ee9cca4c02ed68203f807c65b";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showForecast);
+}
+
 function showTemp(response) {
-  console.log(response.data);
   let currentCity = document.querySelector("#city");
   let tempElement = document.querySelector("#temperature");
   let humidityElement = document.querySelector("#humidity");
@@ -72,6 +79,8 @@ function showTemp(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  displayForecast(response.data.coord);
 }
 function search(city) {
   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
@@ -113,4 +122,3 @@ let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", searchCity);
 
 search("Abuja");
-showForecast();
